e5cdc5f2d5c67bec1939cd6527e061ee438127e6
Eccentric Harry <harinathrao13@gmail.com>
2024-07-03 12:54:12 +0530

    Updated Store with created slices

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";  
 import authSlice from "../features/authSlice";
+import channelSlice from '../features/channelSlice'
+import uiSlice from '../features/uiSlice'
+import videoSlice from '../features/videoSlice'
 
 export const store = configureStore({
     reducer: {
-        auth: authSlice
+        auth: authSlice,
+        video : videoSlice,
+        ui: uiSlice,
+        channel : channelSlice
     }
 })
