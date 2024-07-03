edf665fef16e6a5bb03cdccdb0f2ed213a4784c5
Eccentric Harry <harinathrao13@gmail.com>
2024-07-03 17:03:55 +0530

    chore: Add login and signup pages

---
+++ src/hooks/auth.hook.js
@@ -0,0 +1,46 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  login,
+  logout,
+  getCurrentUser,
+  registerUser,
+  changePassword,
+} from "../api/auth.api";
+
+export const useLogin = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (user) => login(user),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries("currentUser");
+    },
+    retry: 0,
+  });
+};
+
+export const useLogout = () => {
+  return useMutation({
+    mutationFn: () => logout(),
+  });
+};
+
+export const useCurrentUser = () => {
+  return useQuery({
+    queryKey: ["currentUser"],
+    queryFn: () => getCurrentUser(),
+    staleTime: 1000 * 60 * 5,
+    retry: 1,
+  });
+};
+
+export const useRegisterUser = () => {
+  return useMutation({
+    mutationFn: (user) => registerUser(user),
+  });
+};
+
+export const useChangePassword = () => {
+  return useMutation({
+    mutationFn: (data) => changePassword(data),
+  });
+};
