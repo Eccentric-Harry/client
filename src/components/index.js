59c48596bdcd69a57ab39f0af73b69da3fc85531
Eccentric Harry <harinathrao13@gmail.com>
2024-07-06 00:31:43 +0530

    chore: Update front-end title to "VideoTube"

---
+++ src/components/index.js
@@ -0,0 +1,103 @@
+import Logo from "./Logo";
+import Sidebar from "./Sidebar";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import SpButton from "./SpButton";
+import Button from "./Button";
+import Videocard from "./Videocard";
+import Input from "./Input";
+import LoginPopup from "./LoginPopup";
+
+import VideoPlayer from "./VideoPlayer";
+import Like from "./VideoDetails/Like";
+import CommentBox from "./VideoDetails/CommentBox";
+import Comment from "./VideoDetails/Comment";
+import VideolistCard from "./Videocards/VideolistCard";
+import VideoStats from "./VideoStats";
+import UploadVideo from "./UploadVideo/UploadVideo";
+import ProgressBar from "./ProgressBar";
+import DeletePopup from "./DeletePopup";
+import EditVideo from "./EditVideo/EditVideo";
+import VideoPreviewCard from "./VideoPreviewCard";
+import Dropzone from "./Dropzone";
+import { TitleInput, DescriptionInput } from "./TitleAndDescriptionIInput";
+import VideoForm from "./VideoForm";
+import SubscribeButton from "./SubscribeButton";
+import TweetInput from "./Tweet/TweetInput";
+import Tweet from "./Tweet/Tweet";
+import DropDown from "./DropDown";
+import Subscriber from "./Subscriber";
+import ChannelSubscribed from "./ChannelSubscribed";
+import AvatarInput from "./Profile/AvatarInput";
+import CoverImageInput from "./Profile/CoverImageInput";
+import PlaylistDropdown from "./Playlist/PlaylistDropdown";
+import PlaylistCard from "./Playlist/PlaylistCard";
+import PlaylistForm from "./Playlist/PlaylistForm";
+
+import LoadingSpinner from "./Loading/LoadingSpinner";
+import VideoCardSkeleton from "./Loading/VideoCardSkeleton";
+import VideoStatsSkeleton from "./Loading/VideoStatsSkeleton";
+import VideolistCardSkeleton from "./Loading/VideolistCardSkeleton";
+import NextVideoCardSkeleton from "./Loading/NextVideoCardSkeleton";
+import SubscriberSkeleton from "./Loading/SubscriberSkeleton";
+import LoginForm from "./LoginForm";
+
+import NextVideosColumn from "./NextVideos/NextVideosColumn";
+import AllNextVideos from "./NextVideos/AllNextVideos";
+import UserNextVideos from "./NextVideos/UserNextVideos";
+import NextVideoCard from "./NextVideos/NextVideoCard";
+import PlaylistSkeleton from "./Loading/PlaylistSkeleton";
+import MyChannelSkeleton from "./Loading/MyChannelSkeleton";
+import VideoDetailSkeleton from "./Loading/VideoDetailSkeleton";
+
+export {
+  LoginForm,
+  VideoDetailSkeleton,
+  MyChannelSkeleton,
+  PlaylistForm,
+  PlaylistCard,
+  NextVideoCardSkeleton,
+  PlaylistSkeleton,
+  PlaylistDropdown,
+  CoverImageInput,
+  AvatarInput,
+  ChannelSubscribed,
+  SubscriberSkeleton,
+  VideolistCardSkeleton,
+  Subscriber,
+  DropDown,
+  Tweet,
+  TweetInput,
+  SubscribeButton,
+  VideoForm,
+  TitleInput,
+  DescriptionInput,
+  VideoStatsSkeleton,
+  VideoCardSkeleton,
+  Dropzone,
+  VideoPreviewCard,
+  EditVideo,
+  DeletePopup,
+  ProgressBar,
+  UploadVideo,
+  VideoStats,
+  VideolistCard,
+  Comment,
+  CommentBox,
+  Like,
+  LoadingSpinner,
+  VideoPlayer,
+  NextVideoCard,
+  NextVideosColumn,
+  AllNextVideos,
+  UserNextVideos,
+  LoginPopup,
+  Input,
+  Logo,
+  Sidebar,
+  Header,
+  Footer,
+  SpButton,
+  Button,
+  Videocard,
+};
